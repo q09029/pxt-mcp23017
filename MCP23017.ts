a6da5cec0a81d491e89ae645f4070057c48ac06a
q09029 <q09029@shibaura-it.ac.jp>
2019-07-23 12:28:49 +0900

    Updates.

--- MCP23017.ts
+++ MCP23017.ts
@@ -5,20 +5,9 @@
 
 let outputABuffer = 0;
 let outputBBuffer = 0;
+let _addr = 0x20;
 
-enum SET_PORT {
-    //% block=PORT_A
-    A = 0,
-    //% block=PORT_B
-    B = 256
-}
 
-enum REG_PIO {
-    //% block=PORT_A
-    A = 4608,
-    //% block=PORT_B
-    B = 4864
-}
 enum ADDRESS {                     // address for MCP23017 (configurable by tying pins 15,16,17 on the mcp23017 high or low)
     //% block=0x20
     A20 = 0x20,               // 
@@ -44,101 +33,19 @@ enum ADDRESS {                     // address for MCP23017 (configurable by tyin
 "//% weight=100 color=#0fbc12 icon="
 namespace MCP23017 {
     //% block
-    export function clearAllOuputsOn(adress: ADDRESS, port: REG_PIO) {
-        pins.i2cWriteNumber(adress, port + 0, NumberFormat.UInt16BE)
+    export function setAddr(addr: ADDRESS) {
+        _addr = addr;
+    }
+    //% block
+    export function readReg(reg: number): number {
+        pins.i2cWriteNumber(_addr, reg, NumberFormat.Int8LE);
+        return pins.i2cReadNumber(_addr, NumberFormat.Int8LE)
     }
 
     //% block
-    export function setAllOuputsOn(adress: ADDRESS, port: REG_PIO) {
-        pins.i2cWriteNumber(adress, port + 0B11101111, NumberFormat.UInt16BE)
-    }
-
-
-
-    //% block
-    export function setOutputA(bit: number) {
-        outputABuffer = outputABuffer | (1 << bit)
-    }
-
-    //% block
-    export function clearOutputA(bit: number) {
-        let tempMask = 1 << bit
-        tempMask = tempMask ^ 0B11111111
-        outputABuffer = outputABuffer & tempMask
-    }
-
-    //% block
-    export function updateOutputAOn(adress: ADDRESS) {
-        writeNumberToPort(adress, 4608, outputABuffer)
-    }
-
-    //% block 
-    export function clearOutputABuffer() {
-        outputABuffer = 0
-    }
-
-    //% block 
-    export function fillOutputABuffer() {
-        outputABuffer = 0B11111111
-    }
-
-    //% block
-    export function setOutputB(bit: number) {
-        outputBBuffer = outputBBuffer | (1 << bit)
-    }
-
-    //% block
-    export function clearOutputB(bit: number) {
-        let tempMask = 1 << bit
-        tempMask = tempMask ^ 0B11111111
-        outputBBuffer = outputBBuffer & tempMask
-    }
-
-    //% block
-    export function updateOutputBOn(adress: ADDRESS) {
-        writeNumberToPort(adress, 4864, outputBBuffer)
-    }
-
-    //% block
-    export function clearOutputBBuffer() {
-        outputBBuffer = 0
-    }
-
-    //% block
-    export function fillOutputBBuffer() {
-        outputBBuffer = 0B11111111
-    }
-
-    //% block
-    export function writeNumberToPort(adress: ADDRESS, port: REG_PIO, value: number) {
-        pins.i2cWriteNumber(adress, port + value, NumberFormat.UInt16BE)
-    }
-
-    //% block
-    export function setPortAsOutput(adress: ADDRESS, port: SET_PORT) {
-        pins.i2cWriteNumber(adress, port + 0x00, NumberFormat.UInt16BE)
-    }
-
-    //% block
-    export function setPortAsInput(adress: ADDRESS, port: SET_PORT) {
-        pins.i2cWriteNumber(adress, port + 0xFF, NumberFormat.UInt16BE)
-    }
-
-    //% block
-    export function readPort(addr: ADDRESS, reg: REG_PIO): number {
-        pins.i2cWriteNumber(addr, reg, NumberFormat.Int8LE);
-        return pins.i2cReadNumber(addr, NumberFormat.Int8LE)
-    }
-
-    //% block
-    export function readRegister(addr: ADDRESS, reg: REG_PIO): number {
-        pins.i2cWriteNumber(addr, reg+(9<<8), NumberFormat.Int8LE);
-        return pins.i2cReadNumber(addr, NumberFormat.Int8LE)
-    }
-
-    //% block
-    export function ReadNotAnd(addr: ADDRESS, reg: REG_PIO, value: number): boolean {
-        return (!(readRegister(addr, reg) & value))
+    export function writeReg(reg: number, val: number) {
+        pins.i2cWriteNumber(_addr, reg, NumberFormat.Int8LE);
+        pins.i2cWriteNumber(_addr, val, NumberFormat.Int8LE);
     }
 
 }
