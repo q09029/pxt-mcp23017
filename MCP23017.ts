0b6c79a337e49ccc5e9923d83d3b9d0223a265f1
q09029 <q09029@shibaura-it.ac.jp>
2019-07-24 20:48:28 +0900

    Updates.

--- MCP23017.ts
+++ MCP23017.ts
@@ -117,7 +117,7 @@ namespace MCP23017 {
         _addr = addr;
     }
 
-    //% block="GPIO入出力を変更する %val1 %val2"
+    //% block="GPIO入出力を変更する %v"
     //% inlineInputMode=inline
     export function SetIoDir(val1: number, val2: number) {
         writeRegSeq2(MCP23017REG.IODIRA, val1, val2)
